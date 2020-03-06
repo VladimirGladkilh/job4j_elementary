39c22037faad08972bf1b77bee0209485f72c576
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-06 16:08:57 +0500

    0. Операции сравнения[#248510]

---
+++ src/ru/job4j/condition/PointTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.calculator.Fit;
+
+public class PointTest {
+    @Test
+    public void distance(){
+        int x1 = 0;
+        int x2 = 0;
+        int y1 = 2;
+        int y2 = 0;
+        double expected = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,30 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
+        return rsl;
+    }
+
+    public static int rubleToDollar(int value) {
+        int rsl = value / 60; // формула перевода рублей в доллоры.
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToDollar(120);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("120 rubles are " + dollar + " dollar.");
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 eur. Test result : " + passed);
+        int in2 = 120;
+        int expected2 = 2;
+        int out2 = rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("120 rubles are 2 $. Test result : " + passed2);
+    }
+}

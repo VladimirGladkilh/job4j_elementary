9b165cd2fb5bfe5762181fa0c97b2589e2476218
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-11 13:03:58 +0500

    5.8. JavaDoc. Code convention[#248595]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Calculator для сложения двух чисел
+ * @author Gladkih
+ * @since 11/03/2020
+ * @version 1.0
+ */
 public class Calculator {
+    /**
+     * Сложение
+     * @param first первое слагаемое
+     * @param second второе слагаемое
+     */
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);

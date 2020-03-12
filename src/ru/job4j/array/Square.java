b1d52d539468c98297b6c9122dddd883cbbcadd8
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-12 12:34:46 +0500

    6.0.5. Массивы в тестах.[#248535]

--- src/ru/job4j/array/Square.java
+++ src/ru/job4j/array/Square.java
@@ -5,7 +5,9 @@ public class Square {
         int[] rst = new int[bound];
         // заполнить массив через цикл элементами от 0 до bound , возведенными в квадрат
         for (int i=0; i< bound; i++){
-            rst[i] = i*i;
+            rst[i] = i * i; //а есть что то в модуле Math?
+            //есть но требует доп преобразования
+            //rst[i] = (int) Math.pow(i, 2);
         }
         return rst;
     }

570784d166679a9f7a7b210946d84b874368eb82
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-12 11:13:07 +0500

    6.0.2. Заполнение массива.[#248533]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[50];
+        String[] names = new String[4];
+        names[0]="Name 1";
+        names[1]="Name 2";
+        names[2]="Name 3";
+        names[3]="Name 4";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
+
     }
 }

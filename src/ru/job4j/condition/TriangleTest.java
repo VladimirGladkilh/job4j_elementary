19c41cb7010d17ee951f834b47a8261cc17738f5
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-17 15:58:02 +0500

    2.2. Рефакторинг - Площадь треугольника.[#248685]

--- src/ru/job4j/condition/TriangleTest.java
+++ src/ru/job4j/condition/TriangleTest.java
@@ -2,13 +2,20 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 
+import javax.annotation.PostConstruct;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        Point ap = new Point(0, 0);
+        Point bp = new Point(2, 0);
+        Point cp = new Point(0, 2);
+        Triangle triangle = new Triangle(ap, bp, cp);
+        boolean result = triangle.exist(2.0, 2.0, 2.0);
         assertThat(result, is(true));
     }
+
 }
